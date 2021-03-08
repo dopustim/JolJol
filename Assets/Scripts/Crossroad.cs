60773c5d94153e7c3d2bcd4ba45c4ecd57a16a44
Vladimir <dopustimvladimir@gmail.com>
2021-03-08 20:58:08 +0600

    Create some basic logic and Update Unity
    Create logic for objects:
    
    - Crossroad
    - CartDriver
    - MarketStall
    
    Update Unity Editor to 2020.2.7f1

---
+++ Assets/Scripts/Crossroad.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public enum CrossroadType
+{
+    NE, NS, NW, SE, SW, WE
+}
+
+public class Crossroad : MonoBehaviour
+{
+    public CrossroadType actualType;
+    public Sprite[] crossroadSkins;
+
+    void OnMouseDown()
+    {
+        actualType+= 1;
+        actualType = (int)actualType > 5 ? 0 : actualType;
+        SetSkin((int)actualType);
+    }
+
+    void SetSkin(int id)
+    {
+        GetComponent<SpriteRenderer>().sprite = crossroadSkins[id];
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿// using System.Collections.Generic;
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // public static List<Crossroad> crossroads;
+    // public static List<CartDriver> cartDrivers;
+    // public static List<MarketStall> marketStalls;
+}
